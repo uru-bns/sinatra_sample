527502f679ab990e2301147ce1e496d5dbd4f1bc
Urushibara, Akihisa <urushibara@tenso.com>
2015-04-14 20:18:35 +0900

    ERBテンプレート使ってみた

--- sinatra_modular.rb
+++ sinatra_modular.rb
@@ -6,6 +6,16 @@ require 'sinatra/base'
 
 class Application < Sinatra::Base
   get '/' do
-    'Modular Style Sinatra!'
+    erb :index
+  end
+
+  get '/response' do
+    status 404
+    headers 'Content-Type' => 'text/plain'
+    body 'response sample2'
+  end
+
+  get '/dynamic/:name' do
+    "Hi #{params[:name]}"
   end
 end
